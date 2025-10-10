8134d4285c36afcaa91620235a2a1efc5f0f7dcb
cempubar <cempura.bartosz@gmail.com>
2025-10-10 14:28:36 +0200

    ustanowiono połączneie backedn i frontend

--- Webpage/frontend/src/App.jsx
+++ Webpage/frontend/src/App.jsx
@@ -1,10 +1,20 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  const fetchAPI = async () => {
+    const response = await axios.get('/api/utils/health'); // relatywne url dzięki proxy w package.json
+    console.log(response.data.status);
+  };
+
+  useEffect(() => {
+    fetchAPI();
+  }, []);
 
   return (
     <>
--- Webpage/frontend/vite.config.js
+++ Webpage/frontend/vite.config.js
@@ -5,4 +5,13 @@ import react from "@vitejs/plugin-react";
 // https://daisyui.com/docs/install/react/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000", // Flask backend URL
+        changeOrigin: true,
+        secure: false, // optional: disable SSL verification if backend uses HTTP
+      },
+    },
+  },
 });
