fc9c84d4ac5085868fc61b546b8fbd3adc7f8a29
cempubar <cempura.bartosz@gmail.com>
2026-04-07 16:18:11 +0200

    poprawki dla deployment

--- Webpage/frontend/src/api/axios.js
+++ Webpage/frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Tworzymy instancję axios z baseURL "/api", żeby korzystała z proxy Vite
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
